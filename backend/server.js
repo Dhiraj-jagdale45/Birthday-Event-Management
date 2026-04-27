46ff03aafb2f62cfe99cf9b34376317f7fe6521f
Dhiraj Jagdale <dhirajjagdale4@gmail.com>
2026-04-27 14:36:35 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 // ── Middleware ──────────────────────────────────────────────
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://birthday-event-management-1kv2.onrender.com", //"http://localhost:5173"
     methods: ["GET", "POST"],
     credentials: true,
   }),
